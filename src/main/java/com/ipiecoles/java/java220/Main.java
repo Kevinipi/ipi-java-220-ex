6259b3a3e41960d6a8fd4c4429ed281479dfcdc1
Kevinipi <kevin.pose@ipilyon.net>
2019-11-26 10:35:23 +0100

    Création class Main (bac à sable test) pour faire un test d'affichage

---
+++ src/main/java/com/ipiecoles/java/java220/Main.java
@@ -0,0 +1,8 @@
+package com.ipiecoles.java.java220;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue !");
+        System.out.println("le salaire de base est " + Entreprise.SALAIRE_BASE);
+    }
+}
